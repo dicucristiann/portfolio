225ad7b2ab7dfce360f0d5054893170aac585273
dicucristiann <dicucristian24@gmail.com>
2022-07-03 19:59:58 +0300

    second

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -17,7 +17,7 @@ const Hero = () => (
 My name is Cristian Dicu, and I'd like to tell you a little bit about myself.
 I started learning to code a year ago and have since gone on to create some awesome and exciting projects. I am currently employed as a Coupa Analyst at Xoomworks, a division of Accenture, in Cluj-Napoca, Romania, and in my spare time, I explore new web development tools intending to become a front-end web developer. I've made a few projects that you may see while browsing my portfolio and learning more about me.
         </SectionText>
-        <Button onClick={() => window.location = 'https://drive.google.com/uc?export=download&id=131F6IsQQu6rH5qzaNbRI45gx0VFhudki'} >Download CV</Button>
+        <Button onClick={() => window.location = 'https://drive.google.com/uc?export=download&id=1E6aX5BJZ8sQS9PUqOfo05nWlMXqCDCzr'} >Download CV</Button>
       </LeftSection>
     </Section>
   </>
